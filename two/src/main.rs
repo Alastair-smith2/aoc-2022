60d9309a147a2d5a0428017567ca8ab25208e96a
Alastair Smith <Alastair-smith2@users.noreply.github.com>
2022-12-02 08:06:43 +0000

    Title: Make day two more functional
    Body: Use iterators

--- two/src/main.rs
+++ two/src/main.rs
@@ -45,54 +45,103 @@ const WIN_SCORE: u32 = 6;
 // Score for the outcome (0 lost, 3 draw, 6 win)
 
 fn calculate_result(input: &str) -> u32 {
-    let mut total_score = 0;
-    for line in input.lines() {
-        let hand_choices: Vec<&str> = line.split(" ").collect();
-        let hand_pair: (&str, &str) = (hand_choices[0], hand_choices[1]);
+    input
+        .split("\n")
+        .map(|l| l.split(" ").collect())
+        .map(|hand: Vec<&str>| {
+            let hand_pair: (&str, &str) = (hand[0], hand[1]);
 
-        let hand_result = match hand_pair {
-            ("A", "X") => ROCK_VALUE + DRAW_SCORE,
-            ("A", "Y") => PAPER_VALUE + WIN_SCORE,
-            ("A", "Z") => SCISSOR_VALUE + LOST_SCORE,
-            //
-            ("B", "X") => ROCK_VALUE + LOST_SCORE,
-            ("B", "Y") => PAPER_VALUE + DRAW_SCORE,
-            ("B", "Z") => SCISSOR_VALUE + WIN_SCORE,
-            //
-            ("C", "X") => ROCK_VALUE + WIN_SCORE,
-            ("C", "Y") => PAPER_VALUE + LOST_SCORE,
-            ("C", "Z") => SCISSOR_VALUE + DRAW_SCORE,
-            _ => {
-                panic!("Should not have reached this combination")
+            match hand_pair {
+                ("A", "X") => ROCK_VALUE + DRAW_SCORE,
+                ("A", "Y") => PAPER_VALUE + WIN_SCORE,
+                ("A", "Z") => SCISSOR_VALUE + LOST_SCORE,
+                //
+                ("B", "X") => ROCK_VALUE + LOST_SCORE,
+                ("B", "Y") => PAPER_VALUE + DRAW_SCORE,
+                ("B", "Z") => SCISSOR_VALUE + WIN_SCORE,
+                //
+                ("C", "X") => ROCK_VALUE + WIN_SCORE,
+                ("C", "Y") => PAPER_VALUE + LOST_SCORE,
+                ("C", "Z") => SCISSOR_VALUE + DRAW_SCORE,
+                _ => {
+                    panic!("Should not have reached this combination")
+                }
             }
-        };
-        total_score += hand_result;
-    }
-    total_score
+        })
+        .sum()
+
+    // Original
+    // let mut total_score = 0;
+    // for line in input.lines() {
+    //     let hand_choices: Vec<&str> = line.split(" ").collect();
+    //     let hand_pair: (&str, &str) = (hand_choices[0], hand_choices[1]);
+
+    //     let hand_result = match hand_pair {
+    //         ("A", "X") => ROCK_VALUE + DRAW_SCORE,
+    //         ("A", "Y") => PAPER_VALUE + WIN_SCORE,
+    //         ("A", "Z") => SCISSOR_VALUE + LOST_SCORE,
+    //         //
+    //         ("B", "X") => ROCK_VALUE + LOST_SCORE,
+    //         ("B", "Y") => PAPER_VALUE + DRAW_SCORE,
+    //         ("B", "Z") => SCISSOR_VALUE + WIN_SCORE,
+    //         //
+    //         ("C", "X") => ROCK_VALUE + WIN_SCORE,
+    //         ("C", "Y") => PAPER_VALUE + LOST_SCORE,
+    //         ("C", "Z") => SCISSOR_VALUE + DRAW_SCORE,
+    //         _ => {
+    //             panic!("Should not have reached this combination")
+    //         }
+    //     };
+    //     total_score += hand_result;
+    // }
+    // total_score
 }
 
 fn calculate_correct_score(input: &str) -> u32 {
-    let mut total_score = 0;
-    for line in input.lines() {
-        let hand_choices: Vec<&str> = line.split(" ").collect();
-        let elf_choice: ElfChoice = match hand_choices[0] {
-            "A" => ElfChoice::Rock,
-            "B" => ElfChoice::Paper,
-            "C" => ElfChoice::Scissors,
-            _ => panic!("Invalid hand choice!"),
-        };
+    input
+        .split("\n")
+        .map(|l| l.split(" ").collect())
+        .map(|hand: Vec<&str>| {
+            let elf_choice: ElfChoice = match hand[0] {
+                "A" => ElfChoice::Rock,
+                "B" => ElfChoice::Paper,
+                "C" => ElfChoice::Scissors,
+                _ => panic!("Invalid hand choice!"),
+            };
 
-        let my_desired_result = match hand_choices[1] {
-            "X" => DesiredResult::Lose(LOST_SCORE),
-            "Y" => DesiredResult::Draw(DRAW_SCORE),
-            "Z" => DesiredResult::Win(WIN_SCORE),
-            _ => panic!("Invalid desired result"),
-        };
+            let my_desired_result = match hand[1] {
+                "X" => DesiredResult::Lose(LOST_SCORE),
+                "Y" => DesiredResult::Draw(DRAW_SCORE),
+                "Z" => DesiredResult::Win(WIN_SCORE),
+                _ => panic!("Invalid desired result"),
+            };
 
-        let hand_result = calcluate_score_for_desired_outcome(elf_choice, my_desired_result);
-        total_score += hand_result;
-    }
-    total_score
+            return calcluate_score_for_desired_outcome(elf_choice, my_desired_result);
+        })
+        .sum()
+
+    // Original
+    // let mut total_score = 0;
+    // for line in input.lines() {
+    //     let hand_choices: Vec<&str> = line.split(" ").collect();
+    //     let elf_choice: ElfChoice = match hand_choices[0] {
+    //         "A" => ElfChoice::Rock,
+    //         "B" => ElfChoice::Paper,
+    //         "C" => ElfChoice::Scissors,
+    //         _ => panic!("Invalid hand choice!"),
+    //     };
+
+    //     let my_desired_result = match hand_choices[1] {
+    //         "X" => DesiredResult::Lose(LOST_SCORE),
+    //         "Y" => DesiredResult::Draw(DRAW_SCORE),
+    //         "Z" => DesiredResult::Win(WIN_SCORE),
+    //         _ => panic!("Invalid desired result"),
+    //     };
+
+    //     let hand_result = calcluate_score_for_desired_outcome(elf_choice, my_desired_result);
+    //     total_score += hand_result;
+    // }
+    // total_score
 }
 
 fn calcluate_score_for_desired_outcome(
